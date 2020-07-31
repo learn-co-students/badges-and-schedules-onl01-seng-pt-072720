98e6ef211f4a0786f04f5837a115c333ea8aba98
Shalom Kebede <shalomseleshi@gmail.com>
2020-07-31 20:33:59 +0000

    Done.

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -82,6 +82,7 @@ TEXT
       expect(assign_rooms(attendees)).to eq(room_assignments)
     end
     it 'should not hard-code the response' do
+  
       expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
     end
 
