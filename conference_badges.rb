68f1b5e13f6c23e85560590276051b67882d5723
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 05:31:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |attendee|
+    badge_messages << badge_maker(attendee)
+  end 
+  badge_messages
+end 
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |attendee, index|
+    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
+  room_assignments
+end 
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  badges.each do |badge|
+    puts badge
+  end 
+  
+  rooms = assign_rooms(attendees)
+  rooms.each do |room|
+    puts room
+  end
+end
